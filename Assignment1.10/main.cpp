fd33ae1c3e8c76a15dc686b16d0c111769af2117
Lorenzo Chavarria <lorenzoc@iastate.edu>
2018-11-19 15:59:23 -0600

    Decided to do a tic tac toe game for this project.
    Have created the board for the game.

--- Assignment1.10/main.cpp
+++ Assignment1.10/main.cpp
@@ -5,6 +5,51 @@
 
 using namespace std;
 
-main int(){
+//Functions
+void initialize_Game(char board[9]);
+void display_Game(char board[9]);
+//void check_Gameover();
+//void make_Move();
+
+void initialize_Game(char board[9]){
+
+  int i;
+  for(i = 0; i < 9;i++){
+    board[i] = ' ';
+  }
+
+}
+
+void display_Game(char board[9]){
+
+  cout << " TIC" << " TAC" << " TOE" << endl; 
+  cout << " " << endl;
+  cout << " " << board[0] << " | " << board[1] << " | " << board[2] << endl;
+  cout << "------------" << endl;
+  cout << " " << board[3] << " | " << board[4] << " | " << board[5] << endl;
+  cout << "------------" << endl;
+  cout << " " << board[6] << " | " << board[7] << " | " << board[8] << endl;
+  cout << endl;
+}
+
+int main(){
+
+  char gameboard[9];
+  
+  int key = 'a'; 
+  initialize_Game(gameboard);
+
+  display_Game(gameboard);
+  
+  initscr();
+  noecho();
+  while(key!='q'){
+    key = getch();
+    display_Game(gameboard);
+  }
+  endwin();
+  
+  return 0;
+
 
 }
